add706e37bcef5a85dc75c779577ffcb54a9792c
luoxiaoxia <15227856382@163.com>
2021-05-28 09:04:15 +0800

    注释helloworld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 雒小霞
+ * @Date: 2021-05-28 08:42:02
+ * @LastEditors: 雒小霞
+ * @LastEditTime: 2021-05-28 09:04:01
+-->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 

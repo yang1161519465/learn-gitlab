87a88d11d18fd884d2202b9ea48663329b0815e2
luoxiaoxia <15227856382@163.com>
2021-05-28 09:11:30 +0800

    修改Home文件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,26 @@
  * @Author: 雒小霞
  * @Date: 2021-05-28 08:42:02
  * @LastEditors: 雒小霞
- * @LastEditTime: 2021-05-28 09:04:01
+ * @LastEditTime: 2021-05-28 09:10:42
 -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!--  我也要修改Home文件了 -->
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+    <p>2021.5.28  -- lxx</p>
+
   </div>
 </template>
 

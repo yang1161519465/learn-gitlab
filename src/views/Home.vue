093b34ee3e75b8d2b37ea8c3cb1fe8e6c2644c62
杨旭晨 <1161519465@qq.com>
2021-05-28 09:11:42 +0800

    1.修改Home.vue，制造一些冲突

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,24 +3,37 @@
  * @version: 
  * @Author: 雒小霞
  * @Date: 2021-05-28 08:42:02
- * @LastEditors: 雒小霞
- * @LastEditTime: 2021-05-28 09:04:01
+ * @LastEditors: 杨旭晨
+ * @LastEditTime: 2021-05-28 09:11:35
 -->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!-- 添加了一个新功能 -->
+    <div class="container">
+      <div class="left">一些标题</div>
+      <div class="right">
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>

4b2d2e2a20a19e04ace5bc35f0d867f8debaafe0
杨旭晨 <1161519465@qq.com>
2021-05-28 09:34:58 +0800

    1.修改About.vue文件

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
+<!--
+ * @Author: 杨旭晨
+ * @Date: 2021-05-27 22:21:20
+ * @Descripttion: 
+ * @LastEditors: 杨旭晨
+ * @LastEditTime: 2021-05-28 09:34:39
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+export default {
+  /**
+   * 就是想来一点冲突，
+   */
+  name: "About",
+  methods: {
+    xxx() {
+      // 不知名函数
+      console.log("不知道该写点啥");
+    },
+  },
+};
+</script>

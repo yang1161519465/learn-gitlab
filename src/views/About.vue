a8bcca7070d186a2dbb3ca099f031465c08a2782
luoxiaoxia <15227856382@163.com>
2021-05-28 09:35:15 +0800

    script add

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: 雒小霞
+ * @Date: 2021-05-28 08:42:02
+ * @LastEditors: 雒小霞
+ * @LastEditTime: 2021-05-28 09:35:01
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    
   </div>
 </template>
+<style lang="scss" scoped>
+div{
+  border: 1px solid #eee;
+  h1 {
+    text-align: center;
+    color: red;
+    display: inline;
+  }
+}
+
+</style>
